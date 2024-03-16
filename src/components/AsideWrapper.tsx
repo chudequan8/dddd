360cb0c0dd05b68eaa5238fbeec80d962b504788
tookingHome <1412183169@qq.com>
2024-03-16 17:25:47 +0800

    修复aside 和message同时修改状态bug

--- src/components/AsideWrapper.tsx
+++ src/components/AsideWrapper.tsx
@@ -19,7 +19,7 @@ const AsideWrapper: FC<IAsideWrapperProps> = (props) => {
 			data-component-name='AsideWrapper'
 			className={classNames('flex flex-auto flex-col', className, {
 				'ltr:peer-[]:md:pl-[20rem] rtl:peer-[]:md:pr-[20rem]': asideStatus,
-				'ltr:peer-[]:md:pr-[22.5rem] rtl:peer-[]:md:pl-[22.5rem]': asideStatus && messageStatus,
+				'ltr:peer-[]:md:pr-[22.5rem] rtl:peer-[]:md:pl-[22.5rem]': messageStatus,
 				// Mobile Design
 				'ltr:peer-[]:md:pl-[6.225em] rtl:peer-[]:md:pr-[6.225em]': !asideStatus,
 			})}
